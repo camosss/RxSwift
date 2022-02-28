d248c1f97dc41851381acfb4b67c81ddd5a6a873
HoSeong <camosss@naver.com>
2022-02-28 17:00:35 +0900

    [GoodMemo] add: 메모 목록 구현

--- GoodMemo/GoodMemo/ViewController/MemoListViewController.swift
+++ GoodMemo/GoodMemo/ViewController/MemoListViewController.swift
@@ -6,17 +6,36 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import NSObject_Rx
 
 class MemoListViewController: UIViewController, ViewModelBindableType {
 
     var viewModel: MemoListViewModel!
+
+    /// Observable과 TableView를 Binding 하기 때문에 DataSource 연결 X
+    @IBOutlet weak var listTableView: UITableView!
+
+    /// Action을 연결해 구현하는 것이 아닌
+    /// tap속성을 구독하거나, Action 속성에 직접 할당하는 방식
+    @IBOutlet weak var addButton: UIBarButtonItem!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
 
+    /// ViewModel과 View를 Binding
     func bindViewModel() {
-        <#code#>
+        viewModel.title
+            .drive(navigationItem.rx.title)
+            .disposed(by: rx.disposeBag)
+
+        viewModel.memoList
+            .bind(to: listTableView.rx.items(cellIdentifier: "cell")) { row, memo, cell in
+                cell.textLabel?.text = memo.content
+            }
+            .disposed(by: rx.disposeBag)
     }
 }
--- GoodMemo/GoodMemo/ViewModel/CommonViewModel.swift
+++ GoodMemo/GoodMemo/ViewModel/CommonViewModel.swift
@@ -9,6 +9,8 @@ import Foundation
 import RxSwift
 import RxCocoa
 
+/// 의존성을 주입하는 생성자와 Binding에 사용되는 속성과 메서드가 추가
+/// 화면 전환과 메모 저장을 처리 (Scene Coordinator와 MemoryStorage 사용)
 class CommonViewModel: NSObject {
     let title: Driver<String>
     /// Protocol로 선언함으로써 의존성을 쉽게 수정
