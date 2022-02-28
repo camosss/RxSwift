e08b059fcc1c6fdf54c94dbed4fc05110b744798
HoSeong <camosss@naver.com>
2022-02-28 17:40:14 +0900

    [GoodMemo] add: 메모 쓰기 로직 구현

--- GoodMemo/GoodMemo/ViewController/MemoComposeViewController.swift
+++ GoodMemo/GoodMemo/ViewController/MemoComposeViewController.swift
@@ -6,17 +6,55 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import Action
+import NSObject_Rx
 
+/// 새로운 메모 추가, 메모 편집
 class MemoComposeViewController: UIViewController, ViewModelBindableType {
 
     var viewModel: MemoComposeViewModel!
+
+    @IBOutlet weak var cancelButton: UIBarButtonItem!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    @IBOutlet weak var contentTextView: UITextView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        contentTextView.becomeFirstResponder()
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        if contentTextView.isFirstResponder {
+            contentTextView.resignFirstResponder()
+        }
+    }
+
     func bindViewModel() {
-        <#code#>
+        viewModel.title
+            .drive(navigationItem.rx.title)
+            .disposed(by: rx.disposeBag)
+
+        /// 쓰기모드에서는 빈 문자열, 편집모드에서는 편집할 문자열 표시
+        viewModel.initialText
+            .drive(contentTextView.rx.text)
+            .disposed(by: rx.disposeBag)
+
+        /// 취소버튼을 Tap하면 cancelAction에 래핑되어있는 코드 실행
+        cancelButton.rx.action = viewModel.cancelAction
+
+        /// 저장버튼을 Tap하면 TextView에 저장된 문자열을 저장
+        saveButton.rx.tap
+            .throttle(.milliseconds(500), scheduler: MainScheduler.instance) /// 더블 탭 방지
+            .withLatestFrom(contentTextView.rx.text.orEmpty) /// TextView에 입력된 Text를 방출
+            .bind(to: viewModel.saveAction.inputs) /// 방출된 text를 saveAction과 Binding
+            .disposed(by: rx.disposeBag)
     }
 }
--- GoodMemo/GoodMemo/ViewModel/MemoComposeViewModel.swift
+++ GoodMemo/GoodMemo/ViewModel/MemoComposeViewModel.swift
@@ -6,7 +6,55 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+import Action
 
-class MemoComposeViewModel {
-    
+class MemoComposeViewModel: CommonViewModel {
+    /// Compose Scene에 표시할 메모를 저장할 속성
+    /// 새로운 메모는 nil, 편집할 때는 편집할 메모가 저장
+    private let content: String?
+
+    /// Driver: View에 Binding할 수 있도록
+    var initialText: Driver<String?> {
+        return Observable.just(content).asDriver(onErrorJustReturn: "")
+    }
+
+    /// 저장/취소 두가지 Action을 구현 (Action 저장 속성)
+    /// navigationBar에 저장 버튼과 취소 버튼에 두 Action(save, cancel)과 Binding
+    let saveAction: Action<String, Void>
+    let cancelAction: CocoaAction
+
+    /// 저장과 취소 코드를 직접 구현을 하면 ViewModel에서 처리방식이 하나로 고정
+    /// 파라미터로 받으면 이전 화면에서 처리 방식을 동적으로 결정할 수 있다는 장점
+    init(
+        title: String,
+        content: String? = nil,
+        sceneCoordinator: SceneCoordinatorType,
+        storage: MemoStorageType,
+        saveAction: Action<String, Void>? = nil,
+        cancelAction: CocoaAction? = nil
+    ) {
+        self.content = content
+
+        /// saveAction을 옵셔널로 선언하고 한번 더 래핑하여
+        /// 실제로 Action이 전달되었다면 실행(execute) 후 화면을 닫음(close)
+        /// Action이 전달되지 않았다면 화면만 닫음(close)
+        self.saveAction = Action<String, Void> { input in
+            if let action = saveAction {
+                action.execute(input)
+            }
+            return sceneCoordinator.close(animated: true).asObservable().map { _ in }
+        }
+
+        /// cancelAction도 동일
+        self.cancelAction = CocoaAction {
+            if let action = cancelAction {
+                action.execute(())
+            }
+            return sceneCoordinator.close(animated: true).asObservable().map { _ in }
+        }
+        
+        super.init(title: title, sceneCoordinator: sceneCoordinator, storage: storage)
+    }
 }
